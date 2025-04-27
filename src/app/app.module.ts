eff2edca0e938378187302ac64cdf205e4e4bb6e
mateodix <mateo@sherpas.com>
2025-04-27 11:18:50 +0200

    add a middleware to catch the requests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,17 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PdfController } from 'src/pdf/pdf.controller';
 import { PdfService } from 'src/pdf/pdf.service';
+import { LoggerMiddleware } from 'src/middleware/exceptionFilter';
 
 @Module({
   imports: [],
   controllers: [AppController, PdfController],
   providers: [AppService, PdfService],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consomer:MiddlewareConsumer){
+    consomer.apply(LoggerMiddleware).forRoutes('*')
+  }
+}
