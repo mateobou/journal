4c54f8d0d683720a9f2eb2f45d143d4dd45ebd3f
mateodix <mateo@sherpas.com>
2025-04-27 11:24:03 +0200

    [Edit] - Add a DTO validator

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true, // ignore les champs non définis dans le DTO
+    forbidNonWhitelisted: true, // renvoie une erreur si on envoie des champs en trop
+    transform: true, // transforme automatiquement la payload en instance de DTO
+  }));
+
   await app.listen(3003);
 }
 bootstrap();
